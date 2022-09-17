7ce29d642d94a1990f11bdbfe117cb6d63be4673
JongDo737 <jongmin373@gmail.com>
2022-09-17 13:18:26 +0900

    레벨 2 테스트

--- programers/programers/src/level2/Main1.java
+++ programers/programers/src/level2/Main1.java
@@ -1,10 +1,36 @@
 package level2;
 
+import java.util.Scanner;
+
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		System.out.println(sol.solution(s));
 
 	}
 
+}
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+		String[] arr = s.split(" "); // 공백으로 구분
+		int[] numArr = new int[arr.length];
+		int max = -10000000;
+		int min = 10000000;
+		for(int i=0; i< numArr.length; i++) {
+			numArr[i] = Integer.parseInt(arr[i]);
+			if(numArr[i]>max) max = numArr[i];
+			if(numArr[i]<min) min = numArr[i];
+		}
+		answer = min +" "+max;
+        
+        return answer;
+    }
 }
