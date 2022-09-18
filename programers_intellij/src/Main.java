9f495c32f9b532aaf3e2aad94c361e5af7943193
JongDo737 <jongmin373@gmail.com>
2022-09-18 22:14:18 +0900

    mac intellij init

---
+++ programers_intellij/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=0; i<100; i++) {
+            System.out.println("우랙");
+        }
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
