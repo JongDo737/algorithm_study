c6ea201a2620d17ab022857dc460aa5aef6de6b7
JongDo737 <jongmin373@gmail.com>
2022-09-20 18:03:26 +0900

    9/20 알고리즘 공부

--- programers_intellij/src/Main.java
+++ programers_intellij/src/Main.java
@@ -2,11 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        for(int i=0; i<100; i++) {
-            System.out.println("우랙");
-        }
+        Solution sol = new Solution();
         Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(sol.solution(s));
 
+    }
+}
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arStr = s.split(" ");
+        int max = -100000;
+        int min = 100000;
+
+        for(int i=0; i<arStr.length; i++) {
+            int temp = Integer.parseInt(arStr[i]);
+            if(temp >= max) {
+                max = temp;
+            }
+            if(temp <= min) {
+                min = temp;
+            }
+        }
+        answer = min + " " +max;
+
+
+
+        return answer;
     }
 }
