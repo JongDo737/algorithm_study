45cc925adac7721b2b60cfa4522509d0a2a4f3b3
JongDo737 <jongmin373@gmail.com>
2022-09-20 23:18:25 +0900

    피보나치 수 (BigInt 사용

---
+++ programers_intellij/src/Main6.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Solution6 sol = new Solution6();
+        int n = 100;
+        System.out.println(sol.solution(n));
+        
+    }
+}
+class Solution6 {
+    public int solution(int n) {
+        int answer = 0;
+        // n 번째 피보나치 수열을 파악해야함 0,1,1,2,3,5,8...
+        BigInteger left = new BigInteger("0");
+        BigInteger right = new BigInteger("1");
+        for(int i=0; i<n-1; i++){
+            BigInteger temp = left;
+            left = right;
+            right = right.add(temp);
+        }
+        right = right.remainder(BigInteger.valueOf(1234567));
+        answer= right.intValue();
+
+
+        return answer;
+    }
+}
