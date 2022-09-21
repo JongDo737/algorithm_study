0a58bd846cdc9c5e4af3368df836d100af1c5037
JongDo737 <jongmin373@gmail.com>
2022-09-21 20:09:07 +0900

    카펫 성공

--- programers_intellij/src/Main8.java
+++ programers_intellij/src/Main8.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main8 {
     public static void main(String[] args) {
         Solution8 sol = new Solution8();
@@ -9,14 +12,23 @@ public class Main8 {
 }
 class Solution8 {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        // 곱 두개로 나눈 다음 가로-2 * 세로 -2 == yellow dndndndfkgheieosodkdkdk
+        int[] answer = new int[2];
+        // 곱 두개로 나눈 다음 가로-2 * 세로 -2 == yellow
         int total = brown+yellow;
-        for(int i=0; i<total/2+1; i++){
-            
+        List<Integer> divisor = new ArrayList<>();
+        for(int i=3; i<total/2+1; i++){
+            if(total % i==0) divisor.add(i);
         }
+        for(int i=0; i<divisor.size(); i++) {
+            int temp = total/divisor.get(i);
+            if((divisor.get(i)-2)*(temp-2)==yellow) {
+                answer[1]=temp;
+                answer[0]=divisor.get(i);
+            }
 
-
+        }
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
         return answer;
     }
 }
