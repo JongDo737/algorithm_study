3fbde13fbd963b0182f7188a868b4e290648c87e
JongDo737 <jongmin373@gmail.com>
2022-09-21 20:12:25 +0900

    영어 끝말잇기

---
+++ programers_intellij/src/Main9.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main9 {
+    public static void main(String[] args) {
+        Solution9 sol = new Solution9();
+        int n = 10;
+        String[] words = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+        System.out.println(sol.solution(n,words));
+        
+    }
+}
+class Solution9 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
