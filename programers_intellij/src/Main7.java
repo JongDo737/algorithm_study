da96691471db9269b1f77a3f7e0ae4481e52fe97
JongDo737 <jongmin373@gmail.com>
2022-09-21 19:41:05 +0900

    다음 큰 숫자 성공

--- programers_intellij/src/Main7.java
+++ programers_intellij/src/Main7.java
@@ -2,27 +2,26 @@ import java.math.BigInteger;
 
 public class Main7 {
     public static void main(String[] args) {
-        Solution6 sol = new Solution6();
-        int n = 100;
+        Solution7 sol = new Solution7();
+        int n = 78;
         System.out.println(sol.solution(n));
         
     }
 }
-class Solution6 {
+class Solution7 {
     public int solution(int n) {
         int answer = 0;
-        // n 번째 피보나치 수열을 파악해야함 0,1,1,2,3,5,8...
-        BigInteger left = new BigInteger("0");
-        BigInteger right = new BigInteger("1");
-        for(int i=0; i<n-1; i++){
-            BigInteger temp = left;
-            left = right;
-            right = right.add(temp);
+        // n 보다 큼
+        // 2진수 1의 갯수가 같음
+        int oneCount = Integer.toBinaryString(n).replace("0","").length(); //2진수 -> 0없애기 -> 1의 갯수
+        for(int i=n+1; true;i++) {
+            if(Integer.toBinaryString(i).replace("0","").length()== oneCount) {
+                answer = i;
+                break;
+            }
+
+
         }
-        right = right.remainder(BigInteger.valueOf(1234567));
-        answer= right.intValue();
-
-
         return answer;
     }
 }
