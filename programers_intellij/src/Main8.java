0ffc5d461690f605c3e6aaad0a503170c635ce5e
JongDo737 <jongmin373@gmail.com>
2022-09-21 19:59:42 +0900

    카펫

---
+++ programers_intellij/src/Main8.java
@@ -0,0 +1,22 @@
+public class Main8 {
+    public static void main(String[] args) {
+        Solution8 sol = new Solution8();
+        int brown = 10;
+        int yellow = 2;
+        System.out.println(sol.solution(brown,yellow));
+        
+    }
+}
+class Solution8 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        // 곱 두개로 나눈 다음 가로-2 * 세로 -2 == yellow dndndndfkgheieosodkdkdk
+        int total = brown+yellow;
+        for(int i=0; i<total/2+1; i++){
+            
+        }
+
+
+        return answer;
+    }
+}
