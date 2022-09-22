412f08a7a968a04cf0ae04f7aa7a2bc763d27722
JongDo737 <jongmin373@gmail.com>
2022-09-22 17:09:13 +0900

    구명보트 3트만에 오류찾고 성공

--- programers_intellij/src/Main11.java
+++ programers_intellij/src/Main11.java
@@ -4,32 +4,22 @@ import java.util.Stack;
 public class Main11 {
     public static void main(String[] args) {
         Solution11 sol = new Solution11();
-        int[] people = {70, 50, 80,50,60};
-        int limit = 120;
+        int[] people = {70,80,50,50};
+        int limit = 100;
         System.out.println(sol.solution(people,limit));
         
     }
 }
 class Solution11 {
     public int solution(int[] people, int limit) {
+
         int answer = 0;
         // 최소와 최대값을 비교하여 limit-min 값보다 큰사람은 보트 1칸씩 차지하게 하고
         Arrays.sort(people);
-        int heavyPeopleIndex=0;
-        for(int i=0; i<people.length; i++) {
-            if(limit-people[0]<people[i]){
-                heavyPeopleIndex=i;
-                answer +=people.length-i;       //무거운 사람 바로 컷
-                // 0 1 2 3
-                break;
-            }
-        }
-        System.out.println(heavyPeopleIndex);
-        System.out.println(answer);
-        System.out.println((heavyPeopleIndex)%2);
+        int heavyPeopleIndex=people.length;
         int lastPerson = heavyPeopleIndex-1;
         for(int i=0; i<heavyPeopleIndex; i++) {
-
+            System.out.println(people[i] +" "+people[lastPerson]);
             if(i==lastPerson) {
                 answer+=1;
                 break;
@@ -42,6 +32,7 @@ class Solution11 {
             }else {                                     // 무게가 맞지 않으면 무거운사람 혼자 태우고 그 앞사람과 다시 비교 시키기
                 answer+=1;
                 lastPerson-=1;
+                i--;
             }
         }
 
