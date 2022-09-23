ec86960208b9dc33e92b5d40539c9699f268664d
JongDo737 <jongmin373@gmail.com>
2022-09-23 20:06:04 +0900

    1트 런타임 에러

--- programers_intellij/src/Main13.java
+++ programers_intellij/src/Main13.java
@@ -13,7 +13,21 @@ class Solution13
     public int solution(int n, int a, int b)
     {
         int answer = 0;
+        // 12 34 56 78
 
+        while(true) {
+            answer++;
+            if(a+1 == b){
+                break;
+            }
+            if(a%2!=0){
+                a+=1;
+            }if(b%2!=0){
+                b+=1;
+            }
+            a/=2;
+            b/=2;
+        }
 
         return answer;
     }
