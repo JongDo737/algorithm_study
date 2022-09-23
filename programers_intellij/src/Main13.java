e8f4c957f929d4dd58116c1498257028bc8046e4
JongDo737 <jongmin373@gmail.com>
2022-09-23 20:00:56 +0900

    예상 대진표

---
+++ programers_intellij/src/Main13.java
@@ -0,0 +1,20 @@
+public class Main13 {
+    public static void main(String[] args) {
+        Solution13 sol = new Solution13();
+        int n = 8;
+        int a=4;
+        int b = 7;
+        System.out.println(sol.solution(n,a,b));
+
+    }
+}
+class Solution13
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
