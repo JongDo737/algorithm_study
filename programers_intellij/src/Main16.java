36cea078f37d5605835ea25841e33147670a5ec4
JongDo737 <jongmin373@gmail.com>
2022-09-27 18:53:56 +0900

    캐시

---
+++ programers_intellij/src/Main16.java
@@ -0,0 +1,14 @@
+
+public class Main16 {
+    public static void main(String[] args) {
+        int n = 6;
+        Solution16 sol = new Solution16();
+        sol.solution(n);
+    }
+}
+class Solution16 {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        return answer;
+    }
+}
