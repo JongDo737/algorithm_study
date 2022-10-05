929156ef73242ba7e837fe1a709ebb0fe6f651df
JongDo737 <jongmin373@gmail.com>
2022-10-06 00:05:24 +0900

    10/6 알고리즘 스터디

--- programers_intellij/src/Main16.java
+++ programers_intellij/src/Main16.java
@@ -1,18 +1,22 @@
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Main16 {
     public static void main(String[] args) {
         Solution16 sol = new Solution16();
         int cacheSize = 3;
         String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        int result = sol.solution(cacheSize, cities) -> {
+            return 0;
+        };
         sol.solution(cacheSize, cities);
     }
 }
 class Solution16 {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        Sacnner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         return answer;
     }
 }
