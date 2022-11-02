8eb644ac0ae5eba5f06f37b1ee579bb2c57d9a3e
JongDo737 <jongmin373@gmail.com>
2022-11-02 17:22:36 +0900

    streamAPI reduce()

--- programers_intellij/src/Main20.java
+++ programers_intellij/src/Main20.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 public class Main20 {
     public static void main(String[] args) {
         // 퀴즈 5-2
+
+        //map
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 9);
         List<Integer> jaegop = numbers.stream()
                 .map(num -> num * num)
@@ -15,7 +17,7 @@ public class Main20 {
         while (iter.hasNext()) {
             System.out.print(iter.next() + " ");
         }
-
+        // flatmap
         List<Integer> number1 = Arrays.asList(1, 2, 3);
         List<Integer> number2 = Arrays.asList(3, 4);
         List<int[]> pairs = number1.stream()
@@ -24,11 +26,31 @@ public class Main20 {
                 )
                 .collect(Collectors.toList());
 
+
+        //findAny()
         Optional<Integer> someNumbers = numbers.stream()
                 .map(n -> n * n)
                 .filter(n -> n % 2 == 0)
                 .findAny();     // 9
         System.out.println(someNumbers.get());
+
+        //reduce
+        // reduce (초기값 , 두 요소를 조합해서 새로운 값을 만드는 BinaryOperator<T>.
+        int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+        // 자바 8 Integer sum메서드
+        int sum2 = numbers.stream().reduce(0, Integer::sum);
+
+        // 초기값 없는 reduce  -> return Optional객체
+        Optional<Integer> sum3 = numbers.stream().reduce((a, b) -> a + b);
+
+        //퀴즈 5-3  Dish 개수를 map과 reduce로 구하기
+        int sum4 = numbers.stream()
+                .map(dish -> 1)
+                .reduce(0, (a, b) -> a + b);
+        // or
+        Long sum5 = numbers.stream().count();
+
+
     }
 
 
