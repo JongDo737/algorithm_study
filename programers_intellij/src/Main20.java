812d238d770b592e7a7e58b743c8e355f43ae891
JongDo737 <jongmin373@gmail.com>
2022-11-02 17:09:38 +0900

    findAny()

--- programers_intellij/src/Main20.java
+++ programers_intellij/src/Main20.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main20 {
     public static void main(String[] args) {
         // 퀴즈 5-2
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 9);
         List<Integer> jaegop = numbers.stream()
                 .map(num -> num * num)
                 .collect(Collectors.toList());
@@ -18,10 +19,17 @@ public class Main20 {
         List<Integer> number1 = Arrays.asList(1, 2, 3);
         List<Integer> number2 = Arrays.asList(3, 4);
         List<int[]> pairs = number1.stream()
-                                    .flatMap(i -> number2.stream()
-                                                         .map(j -> new int[] {i, j})
-                                    )
-                                    .collect(Collectors.toList());
+                .flatMap(i -> number2.stream()
+                        .map(j -> new int[]{i, j})
+                )
+                .collect(Collectors.toList());
 
+        Optional<Integer> someNumbers = numbers.stream()
+                .map(n -> n * n)
+                .filter(n -> n % 2 == 0)
+                .findAny();     // 9
+        System.out.println(someNumbers.get());
     }
+
+
 }
