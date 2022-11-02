2948f2ca64ec20fb6dfa5c38f7ecc586825b1882
JongDo737 <jongmin373@gmail.com>
2022-11-02 16:52:46 +0900

    streamAPI map flatmap

--- programers_intellij/src/Main20.java
+++ programers_intellij/src/Main20.java
@@ -5,13 +5,23 @@ import java.util.stream.Collectors;
 
 public class Main20 {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        // 퀴즈 5-2
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         List<Integer> jaegop = numbers.stream()
-                                      .map(num -> num*num)
+                .map(num -> num * num)
                 .collect(Collectors.toList());
         Iterator iter = jaegop.iterator();
-        while(iter.hasNext()) {
-            System.out.println(iter.next());
+        while (iter.hasNext()) {
+            System.out.print(iter.next() + " ");
         }
+
+        List<Integer> number1 = Arrays.asList(1, 2, 3);
+        List<Integer> number2 = Arrays.asList(3, 4);
+        List<int[]> pairs = number1.stream()
+                                    .flatMap(i -> number2.stream()
+                                                         .map(j -> new int[] {i, j})
+                                    )
+                                    .collect(Collectors.toList());
+
     }
 }
