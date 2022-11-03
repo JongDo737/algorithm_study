17a867f02052dfe68a4990ca8dd79988c7eaf70f
JongDo737 <jongmin373@gmail.com>
2022-11-03 15:13:50 +0900

    Map.merge

--- programers_intellij/src/Main21.java
+++ programers_intellij/src/Main21.java
@@ -7,11 +7,15 @@ public class Main21 {
         // 집합 펙토리
         Set<String> friendsSet = Set.of("jongmin", "jaydee", "hihhihihi");
 
-        Map<String,Integer> ageOfFriends = Map.of("jongmin",23, "jaydee",21, "hihhihihi",26);
+        Map<String, Integer> ageOfFriends = Map.of("jongmin", 23, "jaydee", 21, "hihhihihi", 26);
 
-        Map<String,Integer> ageOfFriends2 = Map.ofEntries(Map.entry("jongmin",23),
-                                                            Map.entry("jaydee",21),
-                                                            Map.entry("hihhihihi",26));
+        Map<String, Integer> ageOfFriends2 = Map.ofEntries(Map.entry("jongmin", 23),
+                Map.entry("jaydee", 21),
+                Map.entry("hihhihihi", 26));
+        Map<String, Integer> ageOfFriends3 = Map.ofEntries(
+                Map.entry("Raphael", 30),
+                Map.entry("Olivia", 25),
+                Map.entry("Thibaut", 26));
 
 //        for(Iterator<Transaction> iterator = transactions.iterator(); iterator.hasNext();) {
 //            Transaction transaction = iterator.next();
@@ -20,23 +24,26 @@ public class Main21 {
 //            }
 //        }
 
-        for(Map.Entry<String,Integer> entry : ageOfFriends2.entrySet()){
+        for (Map.Entry<String, Integer> entry : ageOfFriends2.entrySet()) {
             String friend = entry.getKey();
             Integer age = entry.getValue();
-            System.out.println(friend+" is "+age+" years old");
+            System.out.println(friend + " is " + age + " years old");
         }
-        ageOfFriends2.forEach((friend, age) -> System.out.println(friend+" is "+age+" years old"));
+        ageOfFriends2.forEach((friend, age) -> System.out.println(friend + " is " + age + " years old"));
 
         ageOfFriends2.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(System.out::println);
 
-        System.out.println(ageOfFriends2.getOrDefault("jongmin",1));        // jongmin이 없으면 1을 반환
+        System.out.println(ageOfFriends2.getOrDefault("jongmin", 1));        // jongmin이 없으면 1을 반환
 //        System.out.println(ageOfFriends2.getOrDefault("jongmin","matrix"));
-        ageOfFriends2.remove("jongmin",24); // 키와 값이 맞으면 삭제
+//        ageOfFriends2.remove("jongmin",24); // 키와 값이 맞으면 삭제
 
-        ageOfFriends2.replaceAll((name, age) -> age+1);
-        ageOfFriends2.replace(key, value);
+//        ageOfFriends2.replaceAll((name, age) -> age+1);
+//        ageOfFriends2.replace(key, value);
+        ageOfFriends3.putAll(ageOfFriends2);
+        Map<String, Integer> everyone = new HashMap<>(ageOfFriends2);
+        ageOfFriends3.forEach((k,v) -> everyone.merge(k,v,(name,age) -> name+age));
+        System.out.println(everyone);
     }
-
 
 
 }
