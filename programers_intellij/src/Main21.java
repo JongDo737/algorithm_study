d3654ba8e5336dad1787ce45e79da2edbbcedcfd
JongDo737 <jongmin373@gmail.com>
2022-11-03 13:30:52 +0900

    asList 와 List.of 차이

--- programers_intellij/src/Main21.java
+++ programers_intellij/src/Main21.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main21 {
+    public static void main(String[] args) {
+        List<String> friends = Arrays.asList("jongmin", "jaydee", "hihhihihi");
+        List<String> friendsList = List.of("jongmin", "jaydee", "hihhihihi");
+
+    }
+
+
+
 }
+
